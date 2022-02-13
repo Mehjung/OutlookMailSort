3e18df4e01ef0a888c78ca460cf2b9069f02c2f3
Mehjung <isla127@googlemail.com>
2022-02-13 14:25:45 +0100

    13.02.2022 Jan 14:25

--- outlookObject.py
+++ outlookObject.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from cmath import e
+import string
 import os
 import win32com.client
 
@@ -6,17 +8,40 @@ class outlookApi(object):
 
     def __init__(self) -> None:
         self.__mapi = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+        self.olFolderInbox = 6
+        self.olNameSpace = self.__mapi
+
+    def printSubjectFromMailin(self, mailname):
+        for x in self.getInboxItemsFromMailIn([mailname]):
+            print (x.Subject)
         
-        
 
-    def getMailInStoreByName(self, name ):
-        pass
+    def getInboxItemsFromMailIn(self, mailInName):
+        for x in mailInName:
+            if self.__mailInExists(x):
+                return self.__getInbox(self.__mapi.Folders(x)).items
 
-    def print(self):
-        for x in self.__mapi.Folders:
-            print (x.Name)
+    def __mailInExists(self, mailInName: string) -> bool:
+        try:
+            self.__mapi.Folders(mailInName)
+            return True
+        except BaseException as e:
+            return not(e.args[0]==-2147352567)
 
+    def __getInbox(self, object):
+        try:
+            return object.Folders("Posteingang")
+        except BaseException as e:
+            return object.Folders("Inbox")
+   
+    def p(self):
+        for x in self.__mapi.folders:
+            print(x.name)
 
+    def find(self, id, mailInBox):
+        return self.__mapi.GetItemFromID(id).Subject
+    
 ol = outlookApi()
-ol.print()
-
+#ol.p()
+#ol.printSubjectFromMailin("Jan.Ehrmantraut@deutschebahn.com")
+print(ol.find("00000000874D1B6A6D17A24AB78977D94C9943410700F97EE50A14DD0D40B89EC2D8805390DC00000000010C0000F97EE50A14DD0D40B89EC2D8805390DC00031CAEC6E20000","Jan.Ehrmantraut@deutschebahn.com"))
