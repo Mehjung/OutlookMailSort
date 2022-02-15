b98a6fdb4dee758abd3109d5b38ef0f6d6e680d0
Mehjung <isla127@googlemail.com>
2022-02-15 16:33:45 +0100

    Jan 15.02.2022 16:33

--- outlookObject.py
+++ outlookObject.py
@@ -5,43 +5,72 @@ import os
 import win32com.client
 
 class outlookApi(object):
+    """
+    Outlook Api Wrapper:
+    """
 
     def __init__(self) -> None:
-        self.__mapi = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-        self.olFolderInbox = 6
-        self.olNameSpace = self.__mapi
+        self.__mapi = self.outlookObject().GetNamespace("MAPI")
 
-    def printSubjectFromMailin(self, mailname):
-        for x in self.getInboxItemsFromMailIn([mailname]):
-            print (x.Subject)
-        
+    def printSubjectFromMailin(self, *mailname):
+        ''' Gibt den EMail Subject Titel der Emails aus dem Posteingang des Arguments auf der Console aus.'''
+        box = self.getInboxItemsFromMailIn(*mailname)
+        if box is not None:
+            for x in box:
+                print (x.Subject)
+        else:
+            print ("No MailBox to print out!")
 
-    def getInboxItemsFromMailIn(self, mailInName):
-        for x in mailInName:
-            if self.__mailInExists(x):
-                return self.__getInbox(self.__mapi.Folders(x)).items
-
-    def __mailInExists(self, mailInName: string) -> bool:
-        try:
-            self.__mapi.Folders(mailInName)
-            return True
-        except BaseException as e:
-            return not(e.args[0]==-2147352567)
-
-    def __getInbox(self, object):
-        try:
-            return object.Folders("Posteingang")
-        except BaseException as e:
-            return object.Folders("Inbox")
-   
-    def p(self):
-        for x in self.__mapi.folders:
-            print(x.name)
+    def getInboxItemsFromMailIn(self, *mailInName):
+        ''' Gibt die Items aus dem Posteingang des Arguments zurück.'''
+        for box in mailInName:
+            if self.__mapi.FolderExists(box):
+                return self.__mapi.Folders(box).Folders("Inbox").items
 
     def find(self, id, mailInBox):
-        return self.__mapi.GetItemFromID(id).Subject
+        ''' gibt ein Item Object basieren auf der Angabe der ID zurueck.'''
+        return self.__mapi.GetItemFromID(id)
     
+    class outlookObject(object):
+        
+        def __init__(self) -> None:
+            self.__outlook = win32com.client.Dispatch("Outlook.Application")
+            self.__aliase = {"Posteingang":"Inbox", "Inbox":"Posteingang"}
+        
+        def Folders(self , name):
+            if self.FolderExists(name):
+                self.__outlook = self.__outlook.Folders(name)
+            if name in self.__aliase and self.FolderExists(self.__aliase[name]):
+                self.__outlook = self.__outlook.Folders(self.__aliase[name])
+            
+            print ("Folder " + name + " nout found!")
+            return self
+       
+        def GetNamespace(self, mapi):
+            self.__outlook = self.__outlook.GetNamespace(mapi)
+            return self
+
+        def FolderExists(self, mailInName: string) -> bool:
+            try:
+                self.__outlook.Folders(mailInName)
+                return True
+            except BaseException as e:
+                return not(e.args[0]==-2147352567)
+        
+        def __getattr__(self, name):
+            try:
+                return getattr(self.__outlook, name)
+            except AttributeError:
+                raise AttributeError(
+                    "'%s' object has no attribute '%s'" % (type(self).__name__, name))
+
+
+
+
 ol = outlookApi()
 #ol.p()
 #ol.printSubjectFromMailin("Jan.Ehrmantraut@deutschebahn.com")
-print(ol.find("00000000874D1B6A6D17A24AB78977D94C9943410700F97EE50A14DD0D40B89EC2D8805390DC00000000010C0000F97EE50A14DD0D40B89EC2D8805390DC00031CAEC6E20000","Jan.Ehrmantraut@deutschebahn.com"))
+#print(ol.find("00000000874D1B6A6D17A24AB78977D94C9943410700F97EE50A14DD0D40B89EC2D8805390DC00000000010C0000F97EE50A14DD0D40B89EC2D8805390DC00031CAEC6E20000","Jan.Ehrmantraut@deutschebahn.com"))
+
+help(ol.find)
+
